c96c6036322eac4d1925d0e15209557223721ccf
CevinxelIsDumb <94770526+CevinxelIsDumb@users.noreply.github.com>
2021-12-12 15:01:25 +1300

    no

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const buttonNew = document.querySelector('.btn--new');
+const player1Score = document.querySelector('#name--0');
+const player2Score = document.querySelector('#name--1');
+const current1Score = document.querySelector('#current--0');
+const current2Score= document.querySelector('#current--1');
 let scoreBoard = [];
 let currentScore =0;
 let isGameRunning
@@ -9,6 +13,10 @@ const startGame = () => {
     currentScore = 0;
     activePlayer =0;
     isGameRunning = true;
+    player1Score.textContent=0;
+    player2Score.textContent=0;
+    current1Score.textContent=0;
+    current2Score.textContent=0;
 }
 startGame();
 
